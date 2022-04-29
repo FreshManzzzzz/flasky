8003669322f1c0dc565b2daa257d132787a239e4
FreshManzzzzz <1027118385@qq.com>
2022-04-29 21:43:33 +0800

    狗书第八章

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
+from flask_login import LoginManager
 from flask_mail import Mail
 from flask_moment import Moment
 from flask_sqlalchemy import SQLAlchemy
@@ -9,6 +10,8 @@ bootstrap = Bootstrap()
 mail = Mail()
 moment = Moment()
 db = SQLAlchemy()
+login_manager = LoginManager()
+login_manager.login_view = 'auth.login'
 
 
 def create_app(config_name):
@@ -20,8 +23,11 @@ def create_app(config_name):
     mail.init_app(app)
     moment.init_app(app)
     db.init_app(app)
+    login_manager.init_app(app)
 
     from .main import main as main_blueprint
+    from .auth import auth as auth_bileprint
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_bileprint, url_prefix='/auth')
 
     return app
